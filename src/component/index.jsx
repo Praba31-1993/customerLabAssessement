f028de57fe36e619983e3bd8bbba3b81ecc7d53b
Praba31-1993 <prabaidymoto@gmail.com>
2024-03-07 22:51:18 +0530

    submit function completed

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {useEffect} from 'react';
 import Backdrop from '@mui/material/Backdrop';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
@@ -28,6 +28,11 @@ export default function TransitionsModal() {
     const [open, setOpen] = React.useState(false);
     const [segmentName, setSegmentName] = React.useState("")
     const [segmentValue, setsegmentValue] = React.useState('');
+    const [inputBox, setInputBox] = React.useState([0])
+    const [totalSegment, setTotalSegment] = React.useState([])
+    const [finalSchema, setFinalSchema] = React.useState([])
+
+
     const UserDatas = [
         { label: "First Name", value: "Rahul" },
         { label: "Last Name", value: "J" },
@@ -37,13 +42,42 @@ export default function TransitionsModal() {
         { label: "City", value: 'chennai' },
         { label: "State", value: "tamilnadu" }
     ];
+
+    useEffect(()=>{
+        handleFinal()
+    },[finalSchema])
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
     const handleChange = (event) => {
         const selectedIndex = event.target.value;
         setsegmentValue(UserDatas[selectedIndex].value);
+        let newTotalSegment = [...totalSegment]
+        newTotalSegment.push(UserDatas[selectedIndex])
+        setTotalSegment(newTotalSegment)
+
     };
-    console.log('seg', segmentValue);
+
+    const handleSubmit = ()=>{
+            
+        const FinalSegmentResult = {
+            "segment_name": segmentName,
+            "schema": totalSegment.map(item => ({ [item.label]: item.value }))
+        };
+        const updatedFinalSchema = [...finalSchema, FinalSegmentResult];
+        setFinalSchema(updatedFinalSchema);
+        handleFinal()
+    }
+
+    const handleFinal = ()=>{
+        console.log('final', finalSchema);
+    }
+    const addSegment = () =>{
+            const existingSegment = [...inputBox];
+        let newArray = []
+        existingSegment.push(newArray)
+        setInputBox(existingSegment)
+
+    }
     return (
         <div>
             <Button onClick={handleOpen}>Save Segment</Button>
@@ -68,39 +102,40 @@ export default function TransitionsModal() {
                                 value={segmentName}
                             />
                         </Typography>
-                        <Typography id="transition-modal-description" sx={{ mt: 2, height: 'auto', width: '100%', border: '1px solid blue' }}>
-                            <h1>Hello</h1>
+                        <Typography id="transition-modal-description" sx={{ mt: 2, height: 'auto', width: '100%', border: '1px solid blue', padding:'10px' }}>
+                            {inputBox.map((inputItem) => (
+                                <div style={{margin:'10px'}}>
+                                    <FormControl fullWidth>
+                                        <InputLabel id="demo-simple-select-label">Segment</InputLabel>
+                                        <Select
+                                            labelId="demo-simple-select-label"
+                                            id="demo-simple-select"
+                                            value={segmentValue.value}
+                                            label="Segment"
+                                            onChange={handleChange}
+                                        >
+                                            {UserDatas.map((data, index) => (
+                                                <MenuItem key={index} value={index}>{data.value}</MenuItem>
+                                            ))}
+                                        </Select>
+                                    </FormControl>
+
+                                </div>
+                            ))}
+
                         </Typography>
 
-                        <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-                            <FormControl fullWidth>
-                                <InputLabel id="demo-simple-select-label">Segment</InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-label"
-                                    id="demo-simple-select"
-                                    value={segmentValue.value}
-                                    label="Segment"
-                                    onChange={handleChange}
-                                >
-                                    {UserDatas.map((data, index) => (
-                                        <MenuItem key={index} value={index}>{data.value}</MenuItem>
-                                    ))}
-                                </Select>
-                            </FormControl>
-                        </Typography>
 
                         <Link
                             component="button"
                             variant="body2"
                             sx={{ marginTop: '20px' }}
-                            onClick={() => {
-                                console.info("schema Added");
-                            }}
+                            onClick={addSegment}
                         >
                             + Add new schema
                         </Link>
                         <Typography id="transition-modal-description" sx={{ mt: 2, display: 'flex', gap: '2em' }}>
-                            <Button variant='outlined'>Save the Segment</Button>
+                            <Button variant='outlined' onClick={handleSubmit}>Save the Segment</Button>
                             <Button variant='contained' onClick={handleClose}>Cancel</Button>
                         </Typography>
 
